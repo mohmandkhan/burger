b01f39ff1b53a86fb5e1cb8660939d09be233bc0
unknown <mohmandkhan@gmail.com>
2021-11-23 11:13:04 +0100

    Added project to github

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+
+class App extends Component {
+  state = {
+    show: true
+  }
+
+
+  render() {
+    return (
+      <div>
+       <Layout>
+        <BurgerBuilder />
+       </Layout>
+      </div>
+    );
+  }
+} 
+
+export default App;
